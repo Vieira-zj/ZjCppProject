95a2a3fd9b195cc1e1d6a3b01dc17e0eb763b523
zhengjin <zivieira@163.com>
2017-03-06 21:55:32 +0800

    Update cpp demo.

--- cpp_demos_03/cpp_sort_demo04.cpp
+++ cpp_demos_03/cpp_sort_demo04.cpp
@@ -2,7 +2,7 @@
 
 int get_first_index_for_sub_string(char target_str[], int target_len, char sub_str[], int sub_len)
 {
-    for (int i = 0; i < target_len; ++i)
+    for (int i = 0; i <= (target_len - sub_len); ++i)
     {
         bool is_found = true;
         for (int j = 0; j < sub_len; ++j)
@@ -12,6 +12,7 @@ int get_first_index_for_sub_string(char target_str[], int target_len, char sub_s
                 break;
             }
         }
+        printf("Iterator at %d\n", i);
         if (is_found) return i;
     }
 
@@ -21,8 +22,8 @@ int get_first_index_for_sub_string(char target_str[], int target_len, char sub_s
 int main(void)
 {
     // get the first index for sub string in target string
-    char target[] = {'x','y','y','y','a','b','c','x'};
-    int target_len = 7;
+    char target[] = {'x','y','y','y','y','b','c','x'};
+    int target_len = 8;
     char sub[] = {'a','b','c'};
     int sub_len = 3;
     int first_idx = get_first_index_for_sub_string(target, target_len, sub, sub_len);
