15a85d873b9d68b94dc32f6eeb053458b29e96aa
zhengjin <zivieira@163.com>
2017-09-30 15:16:55 +0800

    Refactor demos.

--- cpp_demos_03/cpp_my_test_04.cpp
+++ cpp_demos_03/cpp_my_test_04.cpp
@@ -8,6 +8,7 @@ public:
     virtual void func() = 0;
     void func(int x) {cout << "func(int) in base" << endl;}
     void calldis() {display();}
+    virtual ~my_base() {cout << "destroy in base" << endl;}
 private:
     virtual void display() const {cout << "display() in base" << endl;}
 };
@@ -20,31 +21,35 @@ private:
 public:
     my_derived(string str = "ZJ") : s(str) {cout << "derived() " << s << endl;}
     using my_base::func;
-    virtual void func() {cout << "func() in derived" << endl; }
+    virtual void func() {cout << "func() in derived" << endl;}
+    ~my_derived() {cout << "destroy in derived" << endl;}
 };
 
 int my_test_main4(void)
 //int main(void)
 {
+    cout << "method override test:" << endl;
     my_derived d;
     d.func();
     d.func(1);
 
-//    my_derived d;
-//    my_base &r = d;
-//    r.calldis();
+    cout << endl << "virtual method test:" << endl;
+    my_derived d2;
+    my_base &r = d2;
+    r.calldis();
 
-//    my_derived d;
-//    my_base *p = &d;
-//    p->calldis();
+    my_derived d3;
+    my_base *p = &d3;
+    p->calldis();
 
-//    my_base *pn = new my_derived();
-//    pn->calldis();
-//    delete pn;
+    my_base *pn = new my_derived();
+    pn->calldis();
+    delete pn;
 
-//    string str = "zheng";
-//    my_derived d = my_derived(str);
-//    my_derived d1;
+    cout << endl << "constructor test:" << endl;
+    string str = "test";
+    my_derived d4 = my_derived(str);
+    my_derived d5 = str;
 
     return 0;
 }
